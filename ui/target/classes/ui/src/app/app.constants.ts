df07a73d198717fee2c9776d973d243522b68427
Alaa jawhar <alaajawhar123@gmail.com>
2024-09-14 01:07:53 +0300

    remove target folder

--- ui/target/classes/ui/src/app/app.constants.ts
+++
@@ -1,6 +0,0 @@
-export class AppConstants {
-  public static APPLICATION_NAME: string = 'Trading Bot';
-  public static OWNER_COMPANY: string = 'AJProduction';
-  public static OWNER_COMPANY_URL: string = 'www.ajproduction.com';
-  public static TAB_BAR_TEXT: string = 'Test-Portal';
-}
