e36cd97f81ea2a4c6a4489bb3b6abeaa99737e4e
Alaa jawhar <alaajawhar123@gmail.com>
2024-10-02 11:09:31 +0300

    strategy scans ui volume fixed

--- ui/src/main/java/com/amdose/base/App.java
+++ ui/src/main/java/com/amdose/base/App.java
@@ -3,7 +3,7 @@ package com.amdose.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.amdose.base", "com.amdose.database", "com.amdose.pattern.detection.strategies", "io.swagger"})
+@SpringBootApplication(scanBasePackages = {"com.amdose.base", "com.amdose.database", "com.amdose.pattern.detection.strategies", "com.amdose.pattern.detection.services", "io.swagger"})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
